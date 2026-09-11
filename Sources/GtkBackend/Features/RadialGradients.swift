51cbf0f2bdc09d575755f23b7e1abab5e3fc0d78
stackotter <stackotter@stackotter.dev>
2026-09-11 20:13:05 +1000

    Reorganize GtkBackend into a file per backend feature (#770)
    * Reorganize GtkBackend into a file per backend feature
    
    * Move ColorPicker implementation and remove dangling Gradient extension
    
    * Format

--- Sources/GtkBackend/Features/RadialGradients.swift
+++ Sources/GtkBackend/Features/RadialGradients.swift
@@ -1,66 +1,8 @@
 import CGtk
 import Gtk
 @_spi(Backends) import SwiftCrossUI
-import Foundation
 
-extension GtkBackend {
-    public func createLinearGradientWidget() -> Widget {
-        DrawingArea()
-    }
-
-    public func updateLinearGradientWidget(
-        _ widget: Widget,
-        gradient: LinearGradient,
-        withSize size: SIMD2<Int>,
-        in environment: EnvironmentValues
-    ) {
-        let drawingArea = widget as! DrawingArea
-
-        let startPoint = UnitPoint(
-            x: Double(size.x) * gradient.startPoint.x,
-            y: Double(size.y) * gradient.startPoint.y
-        )
-
-        let endPoint = UnitPoint(
-            x: Double(size.x) * gradient.endPoint.x,
-            y: Double(size.y) * gradient.endPoint.y
-        )
-
-        let stops = gradient.gradient.stops
-
-        let colors = stops.map {
-            $0.color.resolve(in: environment)
-        }
-
-        drawingArea.setDrawFunc { [weak self] cairo, _, _ in
-            guard let self else { return }
-
-            let pattern = cairo_pattern_create_linear(
-                startPoint.x,
-                startPoint.y,
-                endPoint.x,
-                endPoint.y
-            )
-
-            for (index, stop) in stops.enumerated() {
-                let color = colors[index]
-                cairo_pattern_add_color_stop_rgba(
-                    pattern,
-                    stop.location,
-                    Double(color.red),
-                    Double(color.green),
-                    Double(color.blue),
-                    Double(color.opacity)
-                )
-            }
-
-            cairo_set_source(cairo, pattern)
-            cairo_rectangle(cairo, 0, 0, Double(size.x), Double(size.y))
-            cairo_fill(cairo)
-            cairo_pattern_destroy(pattern)
-        }
-    }
-
+extension GtkBackend: BackendFeatures.RadialGradients {
     public func createRadialGradientWidget() -> Widget {
         DrawingArea()
     }
@@ -87,9 +29,7 @@ extension GtkBackend {
             $0.color.resolve(in: environment)
         }
 
-        drawingArea.setDrawFunc { [weak self] cairo, _, _ in
-            guard let self else { return }
-
+        drawingArea.setDrawFunc { cairo, _, _ in
             let pattern = cairo_pattern_create_radial(
                 centerX,
                 centerY,
